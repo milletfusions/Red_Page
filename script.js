5cd411334c8c8abad64673a51e798da6788dbdee
milletfusions <praneethmilletfusions@gmail.com>
2025-02-21 15:36:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 if (true) {
-  window.location.href = 'https://bill.gogstbill.com/gst/invoicedetail.php?mode=insert&print=1&invoiceid=257574&searchbillno=&searchstartdate=&searchenddate=&searchtotal=&searchcname=&searchcpersonname=&searchcproductname=&searchcproductgroup=&searchitemnote=&searchremarks=&searchgstin=&sort=DESC&field=id.invoice_id&searchpaymenttype=&searchorderno=&searchorderno_date=&searchlrno=&searchchallanno=&searchextraselect=&searcheinvoice=&searchewaybill=&searchInvType=&serialno=&batchno=&invoiceseries=&searchstatus=&searchbystaff=';
+  window.location.href = 'https://web.whatsapp.com/';
 }
